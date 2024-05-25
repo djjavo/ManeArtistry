059be984e37f92a3ca29c14b2ec7ae4b3a8b9ff4
James Van Hinsbergh <jvanhins@gmail.com>
2024-05-25 20:45:41 +0100

    Attempt to centre everything

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,32 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <div className={styles.container}>
-        <Image
-          className={styles.logo}
-          src='/logo.jpeg'
-          alt='The logo for Mane Artistry by Jay'
-          fill={true}
-        />
+    <div className={styles.topContainer}>
+      <div>
+        <div className={styles.container}>
+          <Image
+            className={styles.logo}
+            src='/logo.jpeg'
+            alt='The logo for Mane Artistry by Jay'
+            fill={true}
+          />
+        </div>
+        <div className={styles.linkContainer}>
+          <li className={styles.links}>
+            <ul>
+              <Link href={'https://www.instagram.com/maneartistrybyjay/'}>
+                Instagram
+              </Link>
+            </ul>
+            <ul>
+              <Link href={'/assets/price-list.pdf'}>Price list</Link>
+            </ul>
+            <ul>
+              <Link href={'mailto:maneartistrybyjay@gmail.com'}>Contact</Link>
+            </ul>
+          </li>
+        </div>
       </div>
-      <div className={styles.linkContainer}>
-        <li className={styles.links}>
-          <ul>
-            <Link href={'https://www.instagram.com/maneartistrybyjay/'}>
-              Instagram
-            </Link>
-          </ul>
-          <ul>
-            <Link href={'/assets/price-list.pdf'}>Price list</Link>
-          </ul>
-          <ul>
-            <Link href={'mailto:maneartistrybyjay@gmail.com'}>Contact</Link>
-          </ul>
-        </li>
-      </div>
-    </>
+    </div>
   );
 }
