2483c1fc4475574f816a3cff3b3f34929ba02990
James Van Hinsbergh <jvanhins@gmail.com>
2024-05-25 20:30:04 +0100

    Add Poetsen One font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,28 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import { Poetsen_One } from 'next/font/google';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+});
+
+const poetsenOne = Poetsen_One({
+  subsets: ['latin'],
+  display: 'swap',
+  //👇 Add variable to our object
+  variable: '--font-poetsen-one',
+  weight: '400',
 });
 
 export const metadata: Metadata = {
-  title: "Mane Artistry by Jay",
-  description: "Mane Artistry by Jay",
+  title: 'Mane Artistry by Jay',
+  description: 'Mane Artistry by Jay',
 };
 
 export default function RootLayout({
@@ -22,8 +31,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${poetsenOne.variable}`}
+      >
         {children}
       </body>
     </html>
